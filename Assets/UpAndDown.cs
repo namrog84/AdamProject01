28feb65fb9a3af504eaecf068c981de8490b7e92
Adam Gorman <Adam@84bit.com>
2013-11-16 00:12:12 -0500

    bouncing boxes test
    Just testing webplayer and bouncing boxes

---
+++ Assets/UpAndDown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpAndDown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        dir = true;
+        System.Random r = new System.Random((int)this.transform.position.y);
+        speed = (float)r.NextDouble()*.3f+.2f;
+	}
+    bool dir;
+    float speed = .3f;
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 a = this.transform.position;
+        if(dir)
+            a.y += speed;
+        else
+            a.y -= speed;
+        if (a.y > 10)
+            dir = false;
+        if (a.y < -5)
+            dir = true;
+
+        this.transform.position = a;
+
+
+	
+	}
+}
