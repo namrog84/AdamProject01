d2b0ca1734b23c47c2d9e35d7cb650eeb459bdab
Adam Gorman <Adam@84bit.com>
2013-11-18 05:59:19 -0500

    indent fix

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
 
-    public Transform target;
+	public Transform target;
 	// Use this for initialization
 	void Start () {
 	    
@@ -11,9 +11,9 @@ public class FollowPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 position = transform.position;
-        position.x = Mathf.Lerp(position.x, target.transform.position.x, Time.deltaTime * 5);
-        position.y = Mathf.Lerp(position.y, target.transform.position.y, Time.deltaTime * 5);
-        transform.position = position;
+		Vector3 position = transform.position;
+		position.x = Mathf.Lerp(position.x, target.transform.position.x, Time.deltaTime * 5);
+		position.y = Mathf.Lerp(position.y, target.transform.position.y, Time.deltaTime * 5);
+		transform.position = position;
 	}
 }
